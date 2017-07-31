1d5a1d62e28b3d61169650433bbf174da43a97d9
Matt Jiles <mjiles1@gmail.com>
2017-07-30 21:28:42 -0400

    initial commit

---
+++ src/components/Order.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Order extends Component {
+	render() {
+		return (
+			<p>
+				Orders
+			</p>
+		);
+	}
+}
+
+export default Order;
---
+++ src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { getFunName } from '../helpers';
+
+class StorePicker extends Component {
+	render() {
+		return (
+			<form action="" className="store-selector">
+				<h2>Please Enter a Store</h2>
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+				<button type="submit">Visit Store</button>
+			</form>
+		);
+	}
+}
+
+export default StorePicker;
