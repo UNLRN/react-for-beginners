f329355dc035696f871afdb53fb2eb73ab49820d
Matt Jiles <mjiles1@gmail.com>
2017-08-01 16:15:15 -0400

    work

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,23 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			fishes: {},
+			order:{}
+		};
+		
+		this.addFish = this.addFish.bind(this);
+	}
+	
+	addFish(fish) {
+		const fishes = {...this.state.fishes};
+		const timestamp = Date.now();
+		fishes[`fish-${timestamp}`] = fish;
+		this.setState({ fishes })
+	}
+
 	render() {
 		return (
 			<div className='catch-of-the-day'>
@@ -11,7 +28,7 @@ class App extends Component {
 					<Header tagline='Fresh Seafood Market'/>
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} />
 			</div>
 		);
 	}
